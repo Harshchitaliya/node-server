059cb2e51f49ed1b4723916229dd001954153c40
Harshchitaliya <chitaliyaharsh09@gmail.com>
2024-11-14 11:57:56 +0530

    work

--- pythonserver/background_removal_service.py
+++ pythonserver/background_removal_service.py
@@ -20,6 +20,6 @@ def remove_background():
         print(f"Error processing image: {e}")
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
